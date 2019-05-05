65571cad740fbc5cfb73401a02a484121e9e44cb
LeoDJ <leandro.spaeth@gmail.com>
2019-05-05 20:40:44 +0200

    implement client management

--- client/mf_client.py
+++ client/mf_client.py
@@ -1,29 +1,56 @@
 import socket
 import json
+import time
+import traceback
+import threading
 
 server_port = 4918
 communication_port = 4919
+heartbeat_interval = 1.0  # s
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-sock.settimeout(0.2)
+sock.settimeout(0)  # make recvfrom non-blocking
 sock.bind(('', communication_port))
 
 server_ip = ''
+running = True
+last_heartbeat = 0
+
+
+def send_heartbeat():
+    if not running:
+        return
+    threading.Timer(heartbeat_interval, send_heartbeat).start()
+    if server_ip:
+        msg = json.dumps({'type': 'heartbeat'}).encode()
+        sock.sendto(msg, (server_ip, server_port))
+    else:
+        print("no server ip " + server_ip)
+
 
 def send_discovery():
     msg = json.dumps({'type': 'discovery'}).encode()
     sock.sendto(msg, ('<broadcast>', server_port))
 
+
+def send_disconnect():
+    if server_ip:
+        msg = json.dumps({'type': 'disconnect'}).encode()
+        sock.sendto(msg, (server_ip, server_port))
+
+
 def handle_msg(data):
     t = data['type']
     if t == 'discovery_response':
+        global server_ip
         server_ip = data['server_ip']
         print('New server IP: ' + server_ip)
 
-running = True
+
 try:
     send_discovery()
+    send_heartbeat()  # start heartbeat timer
     while(running):
         try:
             m = sock.recvfrom(4096)
@@ -35,10 +62,13 @@ try:
                 handle_msg(msg)
             except (json.decoder.JSONDecodeError, IndexError) as e:
                 print("Invalid JSON received - " + str(e))
-        except socket.timeout:
+        except (socket.timeout, BlockingIOError, ConnectionResetError):
             pass
         except (OSError) as e:
-            print(e)
+            # print(e.type, e)
+            traceback.print_exc()
+        time.sleep(0.01)
 except KeyboardInterrupt:
+    send_disconnect()
     running = False
     print('Exiting...')
--- server/mf_server.py
+++ server/mf_server.py
@@ -1,32 +1,62 @@
 import socket
 import json
+import time
+import traceback
+import threading
 
 server_port = 4918
 communication_port = 4919
+heartbeat_check_interval = 1.0  # s
+heartbeat_timeout = 5.0  # s
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(('', server_port))
-s.settimeout(0.2)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.settimeout(0)  # make recvfrom non-blocking
+sock.bind(('', server_port))
 
 my_ip = socket.gethostbyname(socket.gethostname())
+clients = {}
+running = True
+
+
+def check_timeout():
+    if not running:
+        return
+    threading.Timer(heartbeat_check_interval, check_timeout).start()
+    for ip in list(clients):
+        if clients[ip]['last_seen'] < time.time() - heartbeat_timeout:
+            del clients[ip]
+            print("Timeout from client " + ip)
 
 def send_discover_response(ip, port):
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     msg = {'type': 'discovery_response', 'server_ip': my_ip}
     print('Sending response: ' + str(msg))
-    s.sendto(json.dumps(msg).encode(), (ip, port))
+    sock.sendto(json.dumps(msg).encode(), (ip, port))
+
 
 def handle_msg(data):
     t = data['type']
+    global clients
     if t == 'discovery':
         print('Received discovery request from ' + str(data['from_ip']))
         send_discover_response(data['from_ip'], communication_port)
+    elif t == 'heartbeat':
+        clients[data['from_ip']] = {'last_seen': time.time()}
+        # print(clients)
+    elif t == 'disconnect':
+        try:
+            del clients[data['from_ip']]
+        except KeyError:
+            pass
+        print("Disconnect from client " + data['from_ip'])
+        # print(clients)
 
-running = True
+
 try:
+    check_timeout() # start checking for timeouted clients
     while(running):
         try:
-            m = s.recvfrom(4096)
+            m = sock.recvfrom(4096)
             print(str(m[1]) + ': ' + str(m[0]))
             try:
                 msg = json.loads(m[0])
@@ -35,10 +65,13 @@ try:
                 handle_msg(msg)
             except json.decoder.JSONDecodeError:
                 print("Invalid JSON received")
-        except socket.timeout:
+        except (socket.timeout, BlockingIOError, ConnectionResetError):
             pass
         except (OSError) as e:
-            print(e)
+            # print(e.type, e)
+            traceback.print_exc()
+        time.sleep(0.01)
 except KeyboardInterrupt:
     running = False
     print('Exiting...')
+    print(clients)
